c1fb0e3b679e9d46048990954b97307f31d1f535
Jalil Nouri <nouri.6412@gmail.com>
2022-10-01 16:24:26 +0330

    ssssss

--- peer/index.php
+++ peer/index.php
@@ -2,28 +2,50 @@
 
 <head>
   <script src="https://unpkg.com/peerjs@1.3.2/dist/peerjs.min.js"></script>
+  <script src="jquery.min.js"></script>
 </head>
 
 <body>
-  <div></div>
+  <div>
+    <input id="cid" name="cid" value="" placeholder="my id" />
+    <br>
+    <input id="did" name="did" value="" placeholder="dest id" />
+    <br>
+    <button onclick="start_connection()">Start connection</button>
+    <br>
+    <textarea id="mes-box" name="mes-box"></textarea>
+    <button onclick="send_message()">send message</button>
+    <br>
+    <div id="res-box"></div>
+  </div>
   <script>
     var peer = new Peer();
     peer.on('open', function(id) {
       console.log('My peer ID is: ' + id);
+      $('#cid').val(id);
     });
 
-    function start() {
-      var conn = peer.connect('dest-peer-id');
-      conn.on('open', function() {
-        // Receive messages
-        conn.on('data', function(data) {
-          console.log('Received', data);
-        });
+    let conn;
 
-        // Send messages
-        conn.send('Hello!');
+    function start_connection() {
+      conn = peer.connect($('#did').val());
+      conn.on("open", () => {
+        conn.send("hi!");
       });
     }
+
+    function send_message()
+    {
+      conn.send($('#mes-box').val());
+    }
+
+    peer.on("connection", (conn1) => {
+      console.log(conn1);
+      conn1.on("data", (data) => {
+        // Will print 'hi!'
+        console.log(data);
+      });
+    });
   </script>
 </body>
 
