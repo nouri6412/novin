49ba471f6ce5b89eba57425533eda5ec4b6df139
Jalil Nouri <nouri.6412@gmail.com>
2022-10-10 13:02:17 +0330

    homepage

--- homepage-order.php
+++ homepage-order.php
@@ -20,44 +20,30 @@ get_header('order');
         <div class="row">
             <div class="col-12">
                 <div class="title_site mb-2">
-                    <h2>سلام خوش اومدی !</h2>
+                    <h2><?php echo get_field("title"); ?></h2>
                 </div>
-                <p class="text-center mb-5 text-logo">چی می خوای سفارش بدی ؟</p>
+                <p class="text-center mb-5 text-logo"><?php echo get_field("desc"); ?></p>
             </div>
         </div>
     </div>
     <div class="container">
         <div class="row">
-            <div class="col-12 col-sm-6 col-md-4">
-                <a href="#" class="card card-style card-portfolio card-order card-yellow">
-                    <img class="card-img-top img-fluid card-img-top-bradius" src="<?php echo get_template_directory_uri(); ?>/assets/img/1.jpg" alt="Card image cap">
-                   <div class="bg-yellow"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/bg-yellow-2.png" /></div>
-                    <div class="card-body p-2 mb-5">
-                        <h3 class="text-center text-logo">تابلو فرش</h3>
-                    </div>
-                </a>
-            </div>
 
-            <div class="col-12 col-sm-6 col-md-4">
-                <a href="#" class="card card-style card-portfolio  card-order card-yellow">
-                    <img class="card-img-top img-fluid card-img-top-bradius" src="<?php echo get_template_directory_uri(); ?>/assets/img/2.jpg" alt="Card image cap">
-                    <div class="bg-yellow"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/bg-yellow-2.png" /></div>
-
-                    <div class="card-body p-2 mb-5">
-                        <h3 class="text-center text-logo"> دار قالی</h3>
-                    </div>
-                </a>
-            </div>
-            <div class="col-12 col-sm-6 col-md-4">
-                <a href="#" class="card card-style card-portfolio  card-order card-yellow">
-                    <img class="card-img-top img-fluid card-img-top-bradius" src="<?php echo get_template_directory_uri(); ?>/assets/img/3.jpg" alt="Card image cap">
-                    <div class="bg-yellow"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/bg-yellow-2.png" /></div>
-
-                    <div class="card-body p-2 mb-5">
-                        <h3 class="text-center text-logo"> سوپرایز باکس</h3>
-                    </div>
-                </a>
-            </div>
+            <?php
+            $boxs = get_field("boxs");
+            foreach ($boxs as $box) {
+                $item = $box['box'];
+            ?>
+                <div class="col-12 col-sm-6 col-md-4">
+                    <a href="<?php echo $item["link"]; ?>" class="card card-style card-portfolio card-order card-yellow">
+                        <img class="card-img-top img-fluid card-img-top-bradius" src="<?php echo $item["img"]; ?>" alt="<?php echo $item["title"]; ?>">
+                        <div class="bg-yellow"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/bg-yellow-2.png" /></div>
+                        <div class="card-body p-2 mb-5">
+                            <h3 class="text-center text-logo"><?php echo $item["title"]; ?></h3>
+                        </div>
+                    </a>
+                </div>
+            <?php } ?>
 
         </div>
     </div>
