4ae04bbc3b3015b4244a022b91e71bd2179f66e0
Jalil Nouri <nouri.6412@gmail.com>
2022-10-15 15:26:42 +0330

    hhhh

--- homepage-order.php
+++ homepage-order.php
@@ -60,7 +60,7 @@ if (isset($_GET["cat_selected"])) {
 
         <!-- Modal -->
         <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
+            <div class="modal-dialog modal-xl">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
