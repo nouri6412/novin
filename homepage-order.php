958f43faec9e499ce27a05920a240145937a1146
Jalil Nouri <nouri.6412@gmail.com>
2022-10-15 15:33:59 +0330

    hhh

--- homepage-order.php
+++ homepage-order.php
@@ -55,12 +55,12 @@ if (isset($_GET["cat_selected"])) {
         </div>
 <?php if($selected){ ?>
         <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-            Launch demo modal
+             modal
         </button>
 
         <!-- Modal -->
         <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-xl">
+            <div class="modal-dialog modal-fullscreen">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
