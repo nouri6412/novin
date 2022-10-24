d583b260e0db98e26f2f2da551ab24186615046c
Jalil Nouri <nouri.6412@gmail.com>
2022-10-24 13:57:27 +0330

    bbbb

--- homepage-order.php
+++ homepage-order.php
@@ -195,7 +195,7 @@ if (isset($_GET["size_selected"])) {
                 <div class="row">
                     <div class="col-12 col-sm-6 col-md-4 mb-4">
                         <input type="file" name="myfilefield" id="myfilefield" class="form-control" value="">
-                        <button onclick="$('#myfilefield').change()" class="btn btn-primary">انتخاب فایل</button>
+                        <button onclick="$('#myfilefield').click()" class="btn btn-primary">انتخاب فایل</button>
                         <?php wp_nonce_field('myuploadnonce', 'mynonce'); ?>
                     </div>
                     <div class="col-12 col-sm-6 col-md-4 mb-4">
