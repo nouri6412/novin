9405620b2b6d38bc6670cdf3b3ea9d8d024a93ff
Jalil Nouri <nouri.6412@gmail.com>
2022-10-25 16:50:52 +0330

    ddd

---
+++ template-parts/content/content-page.php
@@ -0,0 +1,14 @@
+<main class="content">
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-12">
+                <div class="title_site mb-2">
+                    <h1><?php echo get_the_title(); ?></h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+    <?php echo get_the_content(); ?>
+    </div>
+</main>
