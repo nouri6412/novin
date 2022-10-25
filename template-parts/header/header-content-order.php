c361f4c59f50271ed5b85012eb2f66fd389bf8ff
Jalil Nouri <nouri.6412@gmail.com>
2022-10-25 15:54:55 +0330

    cc

--- template-parts/header/header-content-order.php
+++ template-parts/header/header-content-order.php
@@ -1,26 +1,25 @@
-<a class="scrollTo scroll-To-top " href=".topbar-site "
-        style="visibility: visible; transform: translateY(0px);"></a>
-    <div class="bg-close">
-    </div>
-    <header>
-        <div class="topbar-site ">
-            <div class="container">
-                <div class="row  d-flex justify-content-between align-items-center">
+<a class="scrollTo scroll-To-top " href=".topbar-site " style="visibility: visible; transform: translateY(0px);"></a>
+<div class="bg-close">
+</div>
+<header>
+    <div class="topbar-site ">
+        <div class="container">
+            <div class="row  d-flex justify-content-between align-items-center">
                 <div class="col-6">
-                        <div class="tel_topbar-site  float-right ms-2">
-                            <a href="<?php echo site_url(); ?>" class="md-1"><img class="logo" src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" alt="لوگوی نگاره نوین"> </a>
-                        </div>
+                    <div class="tel_topbar-site  float-right ms-2">
+                        <a href="<?php echo site_url(); ?>" class="md-1"><img class="logo" src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" alt="لوگوی نگاره نوین"> </a>
                     </div>
-                    <div class="col-6">
-                        <div class="tel_topbar-site  float-start ms-2">
-                            <i class="fal fa-phone-alt"></i>
-                            <a href="tel:0914000000" class="me-1">091400000000</a>
-                        </div>
+                </div>
+                <div class="col-6">
+                    <div class="tel_topbar-site  float-start ms-2">
+                        <i class="fal fa-phone-alt"></i>
+                        <a href="tel:0914000000" class="me-1">091400000000</a>
                     </div>
                 </div>
             </div>
         </div>
-    </header>
-    <div class="container">
-    <div class="top"></div>
     </div>
+</header>
+<div class="container">
+    <div class="top"></div>
+</div>
