bb748fd82f751418f034103ddf86a5631c781004
Jalil Nouri <nouri.6412@gmail.com>
2022-10-26 11:21:58 +0330

    mmm

--- woocommerce/cart/cart.php
+++ woocommerce/cart/cart.php
@@ -70,7 +70,9 @@ do_action( 'woocommerce_before_cart' ); ?>
 						if ( ! $product_permalink ) {
 							echo $thumbnail; // PHPCS: XSS ok.
 						} else {
-							printf( '<a href="%s">%s</a>', esc_url( $product_permalink ), $thumbnail ); // PHPCS: XSS ok.
+                            printf( '<a href="#">%s</a>', esc_url( $product_permalink ), $thumbnail ); // PHPCS: XSS ok.
+
+							//printf( '<a href="%s">%s</a>', esc_url( $product_permalink ), $thumbnail ); // PHPCS: XSS ok.
 						}
 						?>
 						</td>
