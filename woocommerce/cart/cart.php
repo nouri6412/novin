9e3b9b2be200a34182c1074a530ba62ac9762321
Home Pc <nouri.6412@gmail.com>
2022-10-27 10:56:11 +0330

    ddd

--- woocommerce/cart/cart.php
+++ woocommerce/cart/cart.php
@@ -56,7 +56,7 @@ do_action( 'woocommerce_before_cart' ); ?>
 										esc_html__( 'Remove this item', 'woocommerce' ),
 										esc_attr( $product_id ),
 										esc_attr( $_product->get_sku() ),
-                                        esc_html__( 'حذف', 'woocommerce' )
+                                        esc_html__( '', 'woocommerce' )
 									),
 									$cart_item_key
 								);
