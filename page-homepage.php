a35d5adac2b80f8bf19bab0cd08a3596ee46e49f
Jalil Nouri <nouri.6412@gmail.com>
2022-11-09 12:37:50 +0330

    yy

--- page-homepage.php
+++ page-homepage.php
@@ -103,7 +103,7 @@ if (isset($_GET["plan_selected"])) {
                     }
                     $product = wc_get_product($item);
                 ?>
-                    <div class="col-6 col-sm-6 col-md-3 mb-4  col-box">
+                    <div class="col-6 col-sm-6 col-md-3 col-box">
                         <a href="<?php echo site_url("?size_selected=" . $item . '&cat_selected=' . $cat_selected) ?>" class="card card-style card-portfolio  card-yellow">
                             <div class="bg-yellow-fix"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/box.png" /></div>
                             <div class="card-body card-body-fix">
