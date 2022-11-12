20fcac02748029d1b5fffe1af46f313c7dfe2e76
Jalil Nouri <nouri.6412@gmail.com>
2022-11-12 10:05:32 +0330

    jjjj

--- footer-order.php
+++ footer-order.php
@@ -153,8 +153,8 @@
 </div>
 
 <script src="<?php echo get_template_directory_uri(); ?>/assets/js/jquery-3.5.0.min.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/popper.min.js" ></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/bootstrap.min.js" ></script>
 <script src="<?php echo get_template_directory_uri(); ?>/assets/js/carousel/owl.carousel.min.js"></script>
 <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/assets/js/main.js?ver=61"></script>
 </body>
