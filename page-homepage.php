09b1e18b7530d97ac45404da0751d840f28fbf91
Jalil Nouri <nouri.6412@gmail.com>
2022-11-12 11:28:52 +0330

    kkk

--- page-homepage.php
+++ page-homepage.php
@@ -37,7 +37,7 @@ if (isset($_GET["category_size"])) {
 }
 
 ?>
-<nav style="--bs-breadcrumb-divider: '<';" aria-label="breadcrumb">
+<nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
   <ol class="breadcrumb">
     <li class="breadcrumb-item"><a href="#">Home</a></li>
     <li class="breadcrumb-item active" aria-current="page">Library</li>
