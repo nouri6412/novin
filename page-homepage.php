bcd5d3d6129af0eac74e5376ddec62708bcf60a1
Jalil Nouri <nouri.6412@gmail.com>
2022-11-13 14:23:00 +0330

    لل

--- page-homepage.php
+++ page-homepage.php
@@ -351,9 +351,9 @@ if (isset($_GET["category_size"])) {
         <div class="container">
 
             <div class="row">
-                <div class="col-0 col-sm-3 col-md-3"></div>
+                <div class="col-0 col-sm-3 col-md-3 select-ghab-panel-option"></div>
                 <h3 data-price="0" id="sum-price-option" class="col-12 col-sm-6 col-md-6 mb-3 select-ghab-panel-option" style="color: green;font-size:18px;"></h3>
-                <div class="col-0 col-sm-3 col-md-3"></div>
+                <div class="col-0 col-sm-3 col-md-3 select-ghab-panel-option"></div>
 
                 <div class="col-0 col-sm-3 col-md-3"></div>
                 <div  class="col-12 col-sm-6 col-md-6 select-ghab-panel-option">
