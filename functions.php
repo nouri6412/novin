a09169d43fa33d96b5e22caf543b238d95c02b92
Jalil Nouri <nouri.6412@gmail.com>
2022-11-13 16:31:19 +0330

    ییی

--- functions.php
+++ functions.php
@@ -263,17 +263,17 @@ function remove_company_name( $fields ) {
 
      unset($fields['billing']['billing_company']);
      unset($fields['billing']['billing_address_2']);
-     $checkout_fields['billing']['billing_first_name']['priority'] = 10;
-     $checkout_fields['billing']['billing_last_name']['priority'] = 20;
-     $checkout_fields['billing']['billing_country']['priority'] = 30;
-     $checkout_fields['billing']['billing_state']['priority'] = 40;
-     $checkout_fields['billing']['billing_city']['priority'] = 50;
-     $checkout_fields['billing']['billing_address_1']['priority'] = 60;
-     $checkout_fields['billing']['billing_postcode']['priority'] = 70;
-     $checkout_fields['billing']['billing_phone']['priority'] = 80;
-     $checkout_fields['billing']['billing_email']['priority'] = 90;
+     $fields['billing']['billing_first_name']['priority'] = 10;
+     $fields['billing']['billing_last_name']['priority'] = 20;
+     $fields['billing']['billing_country']['priority'] = 30;
+     $fields['billing']['billing_state']['priority'] = 40;
+     $fields['billing']['billing_city']['priority'] = 50;
+     $fields['billing']['billing_address_1']['priority'] = 60;
+     $fields['billing']['billing_postcode']['priority'] = 70;
+     $fields['billing']['billing_phone']['priority'] = 80;
+     $fields['billing']['billing_email']['priority'] = 90;
 
-     $checkout_fields['billing']['billing_address_1']['label'] = "آدرس";
+     $fields['billing']['billing_address_1']['label'] = "آدرس";
      return $fields;
 
 }
