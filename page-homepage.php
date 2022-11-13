544febca84fb3ad8f6e12fafc1c7540bc66d255d
Jalil Nouri <nouri.6412@gmail.com>
2022-11-13 11:54:43 +0330

    gg

--- page-homepage.php
+++ page-homepage.php
@@ -375,19 +375,14 @@ if (isset($_GET["category_size"])) {
 
                                 </div>
                             <?php  } else { ?>
-                                <div class="col-12 col-sm-12 col-md-12 mb-4">
-                                    <div class="row">
-                                        <div class="col-12 col-sm-3 col-md-3 mb-2">
-                                            <label class="col-12"><?php echo $item["title"] . ' - ' . $product->get_price_html() ?></label>
-                                            <select class="col-12 col-sm-3 col-md-3" onchange="change_select_option_extra($(this))" data-type="textarea" data-target="f-option-<?php echo $item["product"]; ?>" data-price="<?php echo $product->get_price() ?>">
-                                                <option value="0">نیاز نیست</option>
-                                                <option value="1">نیاز است</option>
-                                            </select>
-                                        </div>
-
-                                        <textarea style="display: none;" placeholder="متن خود را اینجا بنویسید" class="negarenovin-option" class="col-12" data-id="<?php echo $item["product"]; ?>" name="f-option-<?php echo $item["product"]; ?>" id="f-option-<?php echo $item["product"]; ?>"></textarea>
-                                    </div>
+                                <div class="col-6 col-sm-6 col-md-6 mb-4">
+                                    <label><?php echo $item["title"] . ' - ' . $product->get_price_html() ?></label>
+                                    <select onchange="change_select_option_extra($(this))" data-type="textarea" data-target="f-option-<?php echo $item["product"]; ?>" data-price="<?php echo $product->get_price() ?>">
+                                        <option value="0">نیاز نیست</option>
+                                        <option value="1">نیاز است</option>
+                                    </select>
                                 </div>
+                                <textarea style="display: none;" placeholder="متن خود را اینجا بنویسید" class="negarenovin-option" class="col-12" data-id="<?php echo $item["product"]; ?>" name="f-option-<?php echo $item["product"]; ?>" id="f-option-<?php echo $item["product"]; ?>"></textarea>
                             <?php  } ?>
                         <?php } ?>
                     </div>
