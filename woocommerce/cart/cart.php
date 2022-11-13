861db0f3735c971da3231c291e31e39d2ba4613e
Jalil Nouri <nouri.6412@gmail.com>
2022-11-13 15:06:28 +0330

    yyy

--- woocommerce/cart/cart.php
+++ woocommerce/cart/cart.php
@@ -106,7 +106,7 @@ do_action( 'woocommerce_before_cart' ); ?>
 						<td class="product-quantity" data-title="<?php esc_attr_e( 'Quantity', 'woocommerce' ); ?>">
 						<?php
 						if ( $_product->is_sold_individually() || true) {
-							$product_quantity = sprintf( '1 <input type="hidden" name="cart[%s][qty]" value="1" />', $cart_item_key );
+							$product_quantity = sprintf( '<input type="number" name="cart[%s][qty]" value="1" />', $cart_item_key );
 						} else {
 							$product_quantity = woocommerce_quantity_input(
 								array(
