6aeacad43e998fb9acb4fddfa48f527c88a971c5
Jalil Nouri <nouri.6412@gmail.com>
2022-11-14 15:17:25 +0330

    jjjj

--- header-order.php
+++ header-order.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="enamad" content="313675"/>
+        <link href="<?php echo get_template_directory_uri(); ?>/assets/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/respansive.css">
     <link href="<?php echo get_template_directory_uri(); ?>/assets/css/bootstrap.min.css" rel="stylesheet" >
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css?ver=92">
