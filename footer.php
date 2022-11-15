111b5ea60a49d24bb9b050d6dc15182f5e61c22d
Jalil Nouri <nouri.6412@gmail.com>
2022-11-15 10:34:52 +0330

    ddd

--- footer.php
+++ footer.php
@@ -2,43 +2,41 @@
     <div class="area_footer_top">
         <div class="container">
             <div class="row">
+                <?php
+                $data = get_field("sec1", 'option');
+                ?>
                 <div class=" col-sm-6 col-lg-4">
                     <div class="widget_footer">
                         <div class="title_widget_footer">
-                            دسته بندی محصولات
+                            <?php echo  $data["col-1-title"]; ?>
                         </div>
                         <div class="contenet_widget_footer">
                             <ul>
-                                <li><a href="#">بهداشتی آرایشی</a></li>
-                                <li><a href="#">کفش</a></li>
-                                <li><a href="#">لباس</a></li>
-                                <li><a href="#">دیجیتال</a></li>
-                                <li><a href="#">بهداشتی آرایشی</a></li>
-                                <li><a href="#">کفش</a></li>
+                                <?php
+                                foreach ($data["col1"] as $col) {
+                                    $group = $col["link"];
+                                ?>
+                                    <li><a href="<?php echo $group["link"] ?>"><?php echo $group["title"] ?></a></li>
+                                <?php
+                                }
+                                ?>
                             </ul>
                         </div>
                     </div>
                 </div>
                 <div class="col-sm-6 col-lg-4  mt-4 mt-sm-0">
                     <div class="widget_footer">
-                        <div class="title_widget_footer">تماس با ما</div>
+                        <div class="title_widget_footer"> <?php echo  $data["col-2-title"]; ?></div>
                         <div class="contenet_widget_footer">
                             <ul>
-                                <li>
-                                    <span> آدرس: آدرس: ۴۴۵ کوه عدن جاده، کوه بهشت، اوکلند.</span>
-                                </li>
-                                <li>
-                                    <span> شماره تماس: 2135642512</span>
-                                </li>
-                                <li>
-                                    <span> ایمیل: digistore@exmple.com</span>
-                                </li>
-                                <li>
-                                    <span> فکس: 2564521544</span>
-                                </li>
-                                <li>
-                                    <span> زمان کاری: شنبه تا پنچ شنبه ساعت ۸ صبح تا ۱۲ ظهر</span>
-                                </li>
+                                <?php
+                                foreach ($data["col2"] as $col) {
+                                    $group = $col;
+                                ?>
+                                    <li><span><?php echo $group["link"] ?></span>
+                                    <?php
+                                }
+                                    ?>
                             </ul>
                         </div>
                     </div>
@@ -57,27 +55,6 @@
                             </form>
                         </div>
                     </div>
-                    <div class="widget_footer widget_footer_downloadApp  mt-4  mt-md-4  px-sm-3 px-md-3 px-lg-0 w-100">
-                        <div class="title_widget_footer">دانلود برنامه</div>
-                        <div class="contenet_widget_footer">
-                            <div class="download_app_footer d-flex   flex-row  align-items-xl-center flex-sm-column flex-md-row flex-lg-column   flex-xl-row">
-                                <a href="#" class="app_store  d-flex align-items-center justify-content-center ">
-                                    <div class="text_download_app">
-                                        <span>دانلود در </span>
-                                        <span>برنامه استور</span>
-                                    </div>
-                                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/apple.svg" />
-                                </a>
-                                <a href="#" class="app_googlePlay d-flex align-items-center justify-content-center  mt-0 mt-sm-2  mt-md-0 mt-lg-3 mt-xl-0  me-3 me-sm-0 me-md-3 me-lg-0 me-xl-3 ">
-                                    <div class="text_download_app">
-                                        <span>دانلود در </span>
-                                        <span>گوگل پلی </span>
-                                    </div>
-                                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/googlePlay.svg" />
-                                </a>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
@@ -85,97 +62,111 @@
     <div class="area_footer_bottom">
         <div class="container">
             <div class="row area_element-multi">
-                <div class="col-6 col-md-4 col-xl-3 element-multi">
-                    <div class="content-element-multi d-flex align-items-center  mb-3 ">
-                        <div class="icon-element">
-                            <i class="fal fa-truck"></i>
-                        </div>
-                        <div class="detail-element">
-                            <h3>تحویل سریع و رایگان</h3>
-                            <div class="text-multi-element">حمل و نقل رایگان در همه منظور</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-6 col-md-4 col-xl-3 element-multi">
-                    <div class="content-element-multi d-flex align-items-center  mb-3 ">
-                        <div class="icon-element">
-                            <i class="fal fa-user-headset"></i>
-                        </div>
-                        <div class="detail-element">
-                            <h3>پشتیبانی آنلاین ۲۴/۷</h3>
-                            <div class="text-multi-element">پشتیبانی آنلاین ۲۴ ساعت در روز</div>
+                <?php
+                $data = get_field("sec2", 'option');
+                ?>
+                <?php
+                foreach ($data as $col) {
+                    $group = $col["group"];
+                ?>
+                    <div class="col-6 col-md-4 col-xl-3 element-multi">
+                        <div class="content-element-multi d-flex align-items-center  mb-3 ">
+                            <div class="icon-element">
+                                <i class="<?php echo $group["icon"] ?>"></i>
+                            </div>
+                            <div class="detail-element">
+                                <h3><?php echo $group["title"] ?></h3>
+                                <div class="text-multi-element"><?php echo $group["desc"] ?></div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-6 col-md-4 col-xl-3 element-multi mt-4 mt-md-0">
-                    <div class="content-element-multi d-flex align-items-center  mb-3 ">
-                        <div class="icon-element">
-                            <i class="fas fa-undo"></i>
-                        </div>
-                        <div class="detail-element">
-                            <h3>بازگشت پول</h3>
-                            <div class="text-multi-element">تضمین بازگشت به کمتر از ۷ روز</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-6 col-md-4 col-xl-3 element-multi mt-4 mt-xl-0">
-                    <div class="content-element-multi d-flex align-items-center  mb-3 ">
-                        <div class="icon-element">
-                            <i class="fal fa-lock-alt"></i>
-                        </div>
-                        <div class="detail-element ">
-                            <h3>پرداخت امن</h3>
-                            <div class="text-multi-element">امنیت کامل در پرداخت</div>
-                        </div>
-                    </div>
-                </div>
+                <?php
+                }
+                ?>
             </div>
             <hr class="hr_footer_bottom ">
             <div class="row pt-4 pb-4 pb-lg-5 align-items-center">
                 <div class=" col-12 col-lg-8 col-xl-9">
                     <div class="area_menu_footer d-flex  align-items-md-center flex-column flex-md-row ">
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo-main.png" class="img-logoFooter ms-2 mb-2 d-none d-md-block" />
+                        <img src="<?php echo get_field("logo", 'option'); ?>" class="img-logoFooter ms-2 mb-2 d-none d-md-block" />
+                        <?php
+                        $data = get_field("sec3", 'option');
+                        ?>
                         <div class="menu_footer mt-2 mt-md-0">
                             <ul>
-                                <li><a href="#">قوانین سایت</a></li>
-                                <li><a href="#">حساب شما</a></li>
-                                <li><a href="#">نرخ باربری</a></li>
-                                <li><a href="#">بازپرداختی</a></li>
-                                <li><a href="#"> پیگیری سفارش</a></li>
+                                <?php
+                                foreach ($data["links"] as $col) {
+                                    $group = $col["link"];
+                                ?>
+                                    <li><a href="<?php echo $group["link"] ?>"><?php echo $group["title"] ?></a></li>
+                                <?php
+                                }
+                                ?>
                             </ul>
                         </div>
                     </div>
                 </div>
                 <div class="col-12 col-lg-4 col-xl-3 mt-4 mt-lg-0">
                     <div class="area-socialIcon d-flex justify-content-start justify-content-lg-end">
-                        <a href="http://www.facebook.com" class="icon facebook"><span class="tooltip-site">Facebook</span></a>
-                        <a href="http://www.github.com" class="icon instegram"><span class="tooltip-site">instegram</span></a>
-                        <a href="http://www.dribble.com" class="icon linkedin"><span class="tooltip-site">linkedin</span></a>
-                        <a href="http://www.twitter.com" class="icon twitter"><span class="tooltip-site">Twitter</span></a>
-
+                        <?php
+                        foreach ($data["icons"] as $col) {
+                            $group = $col["link"];
+                        ?>
+                            <a href="<?php echo $group["link"] ?>" class="fa fa-<?php echo $group["icon"] ?>"><span class="tooltip-site"><?php echo $group["title"] ?></span></a>
+                        <?php
+                        }
+                        ?>
                     </div>
                 </div>
             </div>
             <div class="row justify-content-between  pb-5 align-items-center">
                 <div class="col-12 col-lg-4 mt-4 mt-lg-0  order-2 order-lg-1">
                     <div class="copyright">
-                        تمامی حقوق سایت محفوظ است
-                    </div>
-                </div>
-                <div class="col-12 col-lg-4 order-1 order-lg-2">
-                    <div class="area_cartimg_footer float-end float-lg-start">
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/images13.png" style="width: 12rem;" />
+                        <?php echo $data["right"] ?>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </footer>
+<div id="bg-white">
+    <div class="box-plan">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/1.jpg" />
+        <button onclick="close_box_plan()" class="btn"><i class="fa fa-window-close"></i></button>
+    </div>
+</div>
+<div id="social-share">
+    <ul class="social-itens hidden">
+        <?php
+        $index = 0;
+        foreach ($data["icons"] as $col) {
+            $index++;
+            if ($index > 4) {
+                $index = 1;
+            }
+            $group = $col["link"];
+        ?>
+            <li>
+                <button onclick="btn_share($(this))" data-href="<?php echo $group["link"] ?>" class="btn-share social-item-<?php echo $index ?>">
+                    <i class="fa fa-<?php echo $group["icon"] ?>"></i>
+                    <span class="btn-share-text">fa fa-<?php echo $group["title"] ?></span>
+                </button>
+            </li>
+        <?php
+        } ?>
+
+    </ul>
+    <div class="social-open-menu">
+        <button class="btn-share"><i class="fa fa-phone"></i></button>
+    </div>
+</div>
+
 <script src="<?php echo get_template_directory_uri(); ?>/assets/js/jquery-3.5.0.min.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/popper.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/bootstrap.min.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/assets/js/carousel/owl.carousel.min.js"></script>
-<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/assets/js/main.js"></script>
+<!-- <script src="<?php echo get_template_directory_uri(); ?>/assets/js/all-js.js"></script> -->
+<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/assets/js/main.js?ver=74"></script>
 </body>
 
 </html>
--- header.php
+++ header.php
@@ -4,12 +4,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="enamad" content="313675"/>
+        <link href="<?php echo get_template_directory_uri(); ?>/assets/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/respansive.css">
-    <link href="<?php echo get_template_directory_uri(); ?>/assets/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+    <link href="<?php echo get_template_directory_uri(); ?>/assets/css/bootstrap.min.css" rel="stylesheet" >
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css?ver=96">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/owlcarousel/owl.carousel.min.css">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/owlcarousel/owl.theme.default.min.css">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/all.min.css">
     <?php wp_head(); ?>
 </head>
 
@@ -18,5 +20,5 @@
     wp_body_open();
     ?>
     <?php
-    get_template_part('template-parts/header/header', 'content');
+    get_template_part('template-parts/header/header', 'content-order');
     ?>
