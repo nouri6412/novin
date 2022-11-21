9f9cd5f49fb3114800e83a7e3a62dafa9af3264f
Jalil Nouri <nouri.6412@gmail.com>
2022-11-21 15:38:27 +0330

    tttt

--- woocommerce/myaccount/my-orders.php
+++ woocommerce/myaccount/my-orders.php
@@ -80,6 +80,7 @@ if ( $customer_orders ) : ?>
 								$actions = wc_get_account_orders_actions( $order );
 
 								if ( ! empty( $actions ) ) {
+									var_dump($order);
 									foreach ( $actions as $key => $action ) { // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
 										echo '<a href="' . esc_url( $action['url'] ) . '" class="button ' . sanitize_html_class( $key ) . '">' . esc_html( $action['name'] ) . '</a>';
 									}
