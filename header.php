06ab6e785cbf26b408158c597f778c8a20c490d3
Jalil Nouri <nouri.6412@gmail.com>
2022-11-21 10:12:08 +0330

    زززز

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <meta name="enamad" content="313675"/>
         <link href="<?php echo get_template_directory_uri(); ?>/assets/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
 
